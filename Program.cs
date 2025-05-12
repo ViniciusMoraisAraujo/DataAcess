642b458567207b0df7c09b9a1b2d2c9085c0afb0
Lonezin <vinidev.morais@gmail.com>
2025-05-12 20:55:17 -0300

    DataAcess

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DataAcess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$; TrustServerCertificate=True;";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+                Console.WriteLine("Connecting to database...");
+                connection.Open();
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+                    
+                    SqlDataReader reader = command.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} -> {reader.GetString(1)}");
+                    }
+                }
+            }
+            
+        }
+    }
+}
+
