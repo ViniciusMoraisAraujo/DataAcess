311b97d94e36f02ffb548d50018b965c02bf5f8b
Lonezin <vinidev.morais@gmail.com>
2025-05-13 15:14:01 -0300

    SqlParameter and Execute in Database

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Data.SqlClient;
+﻿using Dapper;
+using DataAcess.Models;
+using Microsoft.Data.SqlClient;
 
 namespace DataAcess
 {
@@ -7,22 +9,45 @@ namespace DataAcess
         static void Main(string[] args)
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=1q2w3e4r@#$; TrustServerCertificate=True;";
+            var category = new Category();
+            category.Id = Guid.NewGuid();
+            category.Title = "Amazon AWS";
+            category.Url = "Amazon";
+            category.Description = "Category for services AWS";
+            category.Order = 8;
+            category.Summary = "AWS Cloud";
+            category.Featured = false;
+            var insertSql = @"INSERT INTO 
+                                [Category] 
+                            VALUES(
+                                @Id, 
+                                @Title, 
+                                @Url, 
+                                @Summary, 
+                                @Order, 
+                                @Description, 
+                                @Featured
+                                )";
 
             using (var connection = new SqlConnection(connectionString))
             {
                 Console.WriteLine("Connecting to database...");
-                connection.Open();
-                using (var command = new SqlCommand())
+                var rows = connection.Execute(insertSql, new
                 {
-                    command.Connection = connection;
-                    command.CommandType = System.Data.CommandType.Text;
-                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
-                    
-                    SqlDataReader reader = command.ExecuteReader();
-                    while (reader.Read())
-                    {
-                        Console.WriteLine($"{reader.GetGuid(0)} -> {reader.GetString(1)}");
-                    }
+                    category.Id,
+                    category.Title,
+                    category.Url,
+                    category.Summary,
+                    category.Order,
+                    category.Description,
+                    category.Featured
+                });
+                Console.WriteLine($"Rows affected: {rows}");
+                
+                var categories = connection.Query<Category>("SELECT [Id], [Title] FROM [Category]");
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Id} - {item.Title}");
                 }
             }
             
--- obj/Debug/net8.0/DataAcess.AssemblyInfo.cs
+++ obj/Debug/net8.0/DataAcess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAcess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+642b458567207b0df7c09b9a1b2d2c9085c0afb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAcess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAcess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
