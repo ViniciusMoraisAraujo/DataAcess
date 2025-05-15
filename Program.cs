0a738b30fc41411f0649ebceae6a10478116c776
Lonezin <vinidev.morais@gmail.com>
2025-05-15 11:07:18 -0300

    created QueryMultiple

--- Program.cs
+++ Program.cs
@@ -25,7 +25,8 @@ namespace DataAcess
                 //ExecuteScalar(connection);
                 //ReadView(connection);
                 //OneToOne(connection);
-                OneToMany(connection);
+                //OneToMany(connection);
+                //QueryMultiple(connection);
             }
         }
         
@@ -226,6 +227,27 @@ namespace DataAcess
                 {
                     Console.WriteLine($" - {item.Title}");
                 }
+            }
+        }
+
+        static void QueryMultiple(SqlConnection connection)
+        {
+            var query = "SELECT * FROM [Category]; SELECT * FROM [Course]";
+
+            using (var multi = connection.QueryMultiple(query))
+            {
+                var categories = multi.Read<Category>();
+                var courses = multi.Read<Course>();
+
+                foreach (var item in categories)
+                {
+                    Console.WriteLine($"{item.Title}");
+                }
+                foreach (var item in courses)
+                {
+                    Console.WriteLine($"{item.Title}");
+                }
+                
             }
         }
     }
--- obj/Debug/net8.0/DataAcess.AssemblyInfo.cs
+++ obj/Debug/net8.0/DataAcess.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAcess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6aa0ea901efcb2477c8a96d2b723c654747a2bf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+863b844cfe3cd9ae4598b66454923ab90dc3a98d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAcess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAcess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
